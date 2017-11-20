53a19b8b750810c0192cc7dd9ae1e0866f541690
Josef Martinak <josef.martinak@gmail.com>
2017-11-20 12:40:24 +0100

    db commits

--- FeedbackUs_body.php
+++ FeedbackUs_body.php
@@ -95,7 +95,7 @@ class FeedbackUs extends SpecialPage {
 			$ret = 'ok';
 			if( $options || $comment ) {
 				// insert new feedback
-				$dbw->begin();
+				
 				$res = $dbw->insert(
 					'feedbackus',
 					array(
@@ -104,7 +104,7 @@ class FeedbackUs extends SpecialPage {
 						'email' => $email
 					)
 				);
-				$dbw->commit();
+				
 				if ( !$res ) {
 					$ret = 'err';
 				}
@@ -152,13 +152,13 @@ class FeedbackUs extends SpecialPage {
 					// dayips reset if recent day can't be found in db
 					$newdayips = "$dayips,$ip";
 					if ( $dtnow->format('Ymd') != $dbdate->format( 'Ymd' ) ) {
-						$dbw->begin();
+						
 						$res2 = $dbw->update(
 							'articlescores',
 							array( 'day_ips' => '', 'last_inserted' => $now ),
 							array( 'rev_page' => $page_id, 'rev_id' => $rev_id )
 						);
-						$dbw->commit();
+						
 						$dayips = '';
 						$newdayips = $ip;
 					}
@@ -168,7 +168,7 @@ class FeedbackUs extends SpecialPage {
 							$dtnow->format( 'Ymd' ) != $dbdate->format( 'Ymd' ) )
 					{
 						// update rating (condition one ip per revision per day)
-						$dbw->begin();
+						
 						$res2 = $dbw->update(
 							'articlescores',
 							array(
@@ -178,7 +178,7 @@ class FeedbackUs extends SpecialPage {
 								'last_inserted' => $now ),
 							array('id' => $res->id )
 						);
-						$dbw->commit();
+						
 						if ( !$res2 ) {
 							$ok = false;
 							$ret = 'articlescores-error-update';
@@ -191,7 +191,7 @@ class FeedbackUs extends SpecialPage {
 				}
 				else {
 					// insert new revision's rating
-					$dbw->begin();
+					
 					$res2 = $dbw->insert(
 						'articlescores',
 						array(
@@ -202,7 +202,7 @@ class FeedbackUs extends SpecialPage {
 							'day_ips' => $ip,
 							'last_inserted' => $now )
 					);
-					$dbw->commit();
+					
 					if ( !$res2 ) {
 						$ok = false;
 						$ret = 'articlescores-error-insert';
@@ -231,7 +231,7 @@ class FeedbackUs extends SpecialPage {
 			}
 
 			// insert new feedback
-			$dbw->begin();
+			
 			$res = $dbw->insert(
 				'feedbackus',
 				array(
@@ -240,7 +240,7 @@ class FeedbackUs extends SpecialPage {
 					'email' => $email
 				)
 			);
-			$dbw->commit();
+			
 			$ret = 'ok';
 			if ( !$res ) {
 				$ret = 'err';
@@ -270,13 +270,13 @@ class FeedbackUs extends SpecialPage {
 			
 			if( empty( $wgReadOnly ) ) {
 				$this->checkPermissions();
-				$dbw->begin();
+				
 				// remove from DB
 				$res = $dbw->delete(
 					'feedbackus',
 					array( 'id' => $feedback_id )
 				);
-				$dbw->commit();
+				
 				// pager
 				$page = $request->getInt('fuPageNumber',1);
 				if(!$page) $page=1;
